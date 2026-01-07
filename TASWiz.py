fa6773035b420d1a02f1669e1f34356bbac76c2b
James <jamespetts@yahoo.com>
2026-01-07 22:42:44 +0000

    CHANGE: Uppercase the region

--- TASWiz.py
+++ TASWiz.py
@@ -1380,7 +1380,7 @@ class TASWizardDialog(JDialog):
 
         try:
             TBL.SafeSetMemoryValue('RAILWAYCO', '' if company is None else str(company).upper())
-            TBL.SafeSetMemoryValue('REGION', region)
+            TBL.SafeSetMemoryValue('REGION', '' if region is None else str(region).upper())
             TBL.SafeSetMemoryValue('SECTION', section)
         except Exception as ex:
             LogInfo('Failed to set railway details: ' + str(ex))
@@ -1705,6 +1705,13 @@ class TASWizardDialog(JDialog):
                 self._SetStrMem('TASCOVERINKCOLOUR', defaultCoverInk) 
 
                 # ----------------------------- 
+                # Ensure no banding by default (unconditional in wizard): set dark equal to light
+        try:
+            _light = str(TBL.SafeGetOrCreateMemoryValue('TASWTTBANDLIGHT', '255,253,247')).strip()
+        except:
+            _light = '255,253,247'
+        self._SetStrMem('TASWTTBANDDARK', _light)
+
         # Paper colour policy overrides:
         # - TfL always white
         # - London Transport white from 1977
